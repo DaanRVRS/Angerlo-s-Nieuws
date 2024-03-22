dd7d4a279dd111aa2b6b873832f5a1e89d47f39a
BoraEfe <Bora.e@icloud.com>
2024-03-22 11:05:06 +0100

    Create footer

--- Angerlo's Nieuws/src/components/angerloFooter.vue
+++ Angerlo's Nieuws/src/components/angerloFooter.vue
@@ -1,5 +1,32 @@
 <template>
-    <footer></footer>
+    <footer>
+
+      <div class="contact">
+        <div class="contact-title">Contact</div>
+        <p>Tel: 0316-582617</p>
+        <p>E-mail: info @angerlosnieuws.nl </p>
+      </div>
+      <div class="nieuws-container">
+        <div class="nieuws-title">Angerlo's Nieuws</div>
+        <ul>
+          <li><a href="laatste-nieuws">Laatste nieuws</a></li>
+          <li><a href="laatste-edities">Laatste edities</a></li>
+          <li><a href="an-supporter">AN supporter</a></li>
+          <li><a href="contact">Contact</a></li>
+        </ul>
+      </div>  
+      <div class="info-footer">
+        <img class="footer-logo" src="/angerlo-logo.png" alt="Angerlo logo">
+        <div class="info-container">
+          <p>KVK: </p>
+          <p>BTW: </p>
+          <div class="info-title"> Angerlo’s Nieuws is ook verkrijgbaar bij: </div>
+          <p>Kapsalon Judith | Lathum</p>
+          <p>Bakkerij Ten Veen | Angerlo</p>
+          <p>Spar De Molen | Giesbeek</p>
+       </div>
+      </div>
+    </footer>
 </template>
 
 <script>

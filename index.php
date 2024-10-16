8d30167ed29b50bfff77db2a19178fd42aba1e7a
SergioGomezAlvarez <sgmichielr@gmail.com>
2024-10-16 14:42:34 +0200

    Added top news structure

--- index.php
+++ index.php
@@ -12,28 +12,12 @@
 
 get_header(); ?>
 <main class="main-content">
-  <div class="content-wrapper">
-    <div class="main-story">
-      <h2>Main Story</h2>
-    </div>
+  <div class="top-news-container">
+    <div class="news-slider-container">
 
-    <div class="side-stories">
-      <div class="small-block">Small Block 1</div>
-      <div class="small-block">Small Block 2</div>
-      <div class="small-block">Small Block 3</div>
     </div>
-  </div>
-
-  <div class="weather-section">
-    <h3>Het Weer in Angerlo</h3>
-    <div class="weather-days">
-      <div class="day">Maandag - 10°C</div>
-      <div class="day">Dinsdag - 12°C</div>
-      <div class="day">Woensdag - 15°C</div>
-      <div class="day">Donderdag - 13°C</div>
-      <div class="day">Vrijdag - 14°C</div>
-      <div class="day">Zaterdag - 16°C</div>
-      <div class="day">Zondag - 17°C</div>
+    <div class="small-news-container">
+      
     </div>
   </div>
 </main>

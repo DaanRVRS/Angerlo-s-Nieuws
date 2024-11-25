66d216d17758db257907342e35be264e3a45b751
DaanRVRS <dfpreuvers@gmail.com>
2024-11-25 11:38:04 +0100

    added header to theme

--- header.php
+++ header.php
@@ -1,79 +1,39 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Newsmatic
- */
-use Newsmatic\CustomizerDefault as ND;
-?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	<?php wp_head(); ?>
 </head>
 <!-- HIER MOET DE NIEUWE HEADER HTML -->
-<body <?php body_class(); ?> <?php newsmatic_schema_body_attributes(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'newsmatic' ); ?></a>
-	<?php
-		if( did_action( 'elementor/loaded' ) && class_exists( 'Nekit_Render_Templates_Html' ) ) :
-			$Nekit_render_templates_html = new Nekit_Render_Templates_Html();
-			if( $Nekit_render_templates_html->is_template_available('header') ) {
-				$header_rendered = true;
-				echo $Nekit_render_templates_html->current_builder_template();
-			} else {
-				$header_rendered = false;
-			}
-		else :
-			$header_rendered = false;
-		endif;
-
-		if( ! $header_rendered ) :
-		?>
-			<div class="newsmatic_ovelay_div"></div>
-			<?php
-				/**
-				 * hook - newsmatic_page_prepend_hook
-				 * 
-				 * @package Newsmatic
-				 * @since 1.0.0
-				 */
-				do_action( "newsmatic_page_prepend_hook" );
-			?>
-			
-			<header id="masthead" class="site-header layout--default layout--one">
-				<?php
-					/**
-					 * Function - newsmatic_top_header_html
-					 * 
-					 * @since 1.0.0
-					 * 
-					 */
-					newsmatic_top_header_html();
-
-					/**
-					 * Function - newsmatic_header_html
-					 * 
-					 * @since 1.0.0
-					 * 
-					 */
-					newsmatic_header_html();
-				?>
-			</header><!-- #masthead -->
-			
-			<?php
-			/**
-			 * function - newsmatic_after_header_html
-			 * 
-			 * @since 1.0.0
-			 */
-			newsmatic_after_header_html();
-		endif;
+<header>
+	<div class="left-container">
+		<div class="logo-container">
+			<a href="/">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="angerlo's logo">
+			</a>
+			<div class="vertical-divider"></div>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/laatste-nieuws">Laatste Nieuws</a></li>
+				<li><a href="/laatste-nieuws">Laatste Edities</a></li>
+				<li><a href="/laatste-nieuws">AN Supporter</a></li>
+				<li><a href="/contact">Contact</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div class="right-container">
+		<?php if ( is_user_logged_in() ) : ?>
+			<div class="user-info">
+				<span class="loggedin-username">Welkom, <?php echo wp_get_current_user()->display_name; ?>!</span>
+			</div>
+		<?php else : ?>
+			<a href="<?php echo wp_login_url(); ?>" class="login-button">Inloggen</a>
+		<?php endif; ?>
+	</div>
+</header>
+</html>
